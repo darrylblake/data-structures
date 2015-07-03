6eba3e09e0965d64f761e803301f235cf30f6e21
Hack Reactor Students <accounts@hackreactor.com>
2015-07-03 12:08:01 -0700

    added recursive solution in functional-shared manner

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,27 +2,45 @@ var Tree = function(value){
   var newTree = {};
   newTree.value = value;
 
-  // your code here
-  newTree.children = null;  // fix me
+  _.extend(newTree, treeMethods);
+
+  newTree.children = [];  // fix me
 
   return newTree;
 };
 
 
 
-
-
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+	this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target){
-
+	var result = false;
+	
+	var checkChildren = function(tree) {
+		// base case
+		if (tree.value === target) {
+			result = true;
+		}
+		// recursive case
+		if (tree.children) {
+			_.each(tree.children, function(child) {
+				checkChildren(child);
+			});
+		}
+	}
+	
+	checkChildren(this);
+	
+	return result;
 };
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+ // 
