b274a7f4dc593f1362b0c98c37c53612be2dfa71
Darryl Blake <darryl@nikordi.com>
2015-07-05 12:03:52 -0700

    Pseudoclassical: Stack and Queue - Completed

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,23 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this._storage = {};
+  this._head = 0;
+  this._tail = 0;
 };
 
+Queue.prototype.size = function() {
+  return this._tail - this._head;
+}
 
+Queue.prototype.enqueue = function(value) {
+  this._storage[this._tail] = value;
+  this._tail++;
+}
+
+Queue.prototype.dequeue = function() {
+  var dequeued = this._storage[this._head];
+  delete this._storage[this._head];
+  this.size() && this._head++;
+  return dequeued;
+}
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,24 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this._size = 0;
+  this._storage = {};
 };
 
+
+Stack.prototype.size = function() {
+  return this._size;
+}
+
+Stack.prototype.push = function(value) {
+  this._size++;
+  this._storage[this._size]  = value;
+}
+
+Stack.prototype.pop = function() {
+  var popped = this._storage[this._size];
+  delete this._storage[this._size];
+  this._size && this._size--;
+  return popped;
+}
 
