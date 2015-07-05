7ef9637f33200e8c716cc867d89e41ed8123bcce
Darryl Blake <darryl@nikordi.com>
2015-07-05 15:28:35 -0700

    Linked List - Completed

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,35 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var node = Node(value);
+    // setting the head to the first entered node
+    if (list.head === null) {
+      list.head = node;
+    }
+    // Setting old tail next to new node.
+    if (list.tail !== null)
+      list.tail.next = node;
+    list.tail = node;
   };
 
   list.removeHead = function(){
+    var result = list.head.value;
+    list.head = list.head.next
+    return result;
   };
 
   list.contains = function(target){
+    node = list.head;
+    result = false;
+    while (node !== null) {
+      if (node.value === target) {
+        result = true;
+        break;
+      }
+      // Setting node to next node
+      node = node.next;
+    }
+    return result;
   };
 
   return list;
