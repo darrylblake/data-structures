7ee0b0e0bb2fd703e69ed360ff278be9e87b0fc2
Darryl Blake <darryl@nikordi.com>
2015-07-05 15:28:54 -0700

    Tree - Completed

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,26 +2,38 @@ var Tree = function(value){
   var newTree = {};
   newTree.value = value;
 
-  // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
+
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
 
 
-
-
-
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  var child = Tree(value);
+  this.children.push(child);
 };
 
+// Time Complexity: Exponential
 treeMethods.contains = function(target){
-
+  var result = false;
+  function subroutine(node) {
+    // Base case
+    if (node.value === target)
+      result = true;
+    // Recursive case
+    if (node.children) {
+      _.each(node.children, function(child) {
+        subroutine(child);
+      });
+    }
+  }
+  subroutine(this);
+  return result;
 };
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
