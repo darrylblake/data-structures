d05cc9d39c61bc53bc76fa6db44920a0c58a1a7d
Darryl Blake <darryl@nikordi.com>
2015-07-05 10:16:56 -0700

    Functional: Stack - Completed

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,28 @@ var Stack = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var length = 0;
 
   // Implement the methods below
   someInstance.push = function(value){
+    storage[length] = value;
+    length++;
   };
 
   someInstance.pop = function(){
+    if (length > 0) {
+      length--;
+      // getting popped value to return later
+      var popped = storage[length];
+      // prevents memory leak?
+      delete storage[length];
+      return popped;
+    }
+
   };
 
   someInstance.size = function(){
+    return length;
   };
 
   return someInstance;
